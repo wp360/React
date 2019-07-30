3fb36009941a2e488b987249aa1f08a552564390
wp360 <2897406527@qq.com>
2019-07-31 00:32:26 +0800

    商家详情tab

--- src/page/detail/Main/Main.jsx
+++ src/page/detail/Main/Main.jsx
@@ -1,19 +1,36 @@
 import './Main.scss';
 import React from 'react';
 import {connect} from 'react-redux';
+import NavHeader from 'component/NavHeader/Navheader';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  renderTabs() {
+    let tabs = this.props.tabs;
+    return tabs.map((item) => {
+      return (
+        <div className="tab-item active" key={item.key}>{item.name}</div>
+      )
+    })
+  }
+
   render() {
     return (
-      <div>
-        {1}
+      <div className="detail">
+        <NavHeader title={'黄焖鸡'} />
+        <div className="tab-bar">
+          {this.renderTabs()}
+        </div>
       </div>
     );
   }
 }
 
-export default connect()(Main);
+export default connect(
+  state => ({
+    tabs: state.tabReducer.tabs
+  })
+)(Main);
