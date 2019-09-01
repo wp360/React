e277932f3a49372830a26442d07f7032a95e5c77
wp360 <2897406527@qq.com>
2019-09-02 03:49:23 +0800

    左侧导航

--- src/admin.js
+++ src/admin.js
@@ -3,18 +3,19 @@ import { Row, Col } from 'antd';
 import NavLeft from './components/NavLeft';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import './style/common.less';
 
 export default class Admin extends React.Component {
   render() {
     return(
       <div>
-          <Row>
-            <Col span={3}>
+          <Row className="container">
+            <Col span={3} className="nav-left">
               <NavLeft/>
             </Col>
-            <Col span={21}>
+            <Col span={21} className="main">
               <Header/>
-              <Row>
+              <Row className="content">
                 内容
               </Row>
               <Footer/>
