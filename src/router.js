67e5d7917cb87e8ed9b89e90c5032aef6f6552b6
wp360 <2897406527@qq.com>
2019-09-13 23:29:27 +0800

    spin加载中

--- src/router.js
+++ src/router.js
@@ -5,6 +5,7 @@ import Login from './pages/login'
 import Admin from './admin'
 import Buttons from './pages/ui/buttons'
 import Modals from './pages/ui/modals'
+import Loadings from './pages/ui/loadings'
 import NoMatch from './pages/nomatch/index'
 
 export default class IRouter extends React.Component {
@@ -18,6 +19,7 @@ export default class IRouter extends React.Component {
               <Switch>
                 <Route path="/admin/ui/buttons" component={Buttons} />
                 <Route path="/admin/ui/modals" component={Modals} />
+                 <Route path="/admin/ui/loadings" component={Loadings} />
                 <Route component={NoMatch} />
               </Switch>
             </Admin>
